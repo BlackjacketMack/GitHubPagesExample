9906c40b245c37522f932491415b383fdaf63895
Rob Mackenzie <rob@jeraconcepts.com>
2023-02-27 22:32:35 -0500

    Working on posts

---
+++ GitHubPagesExample/Pages/Blog/_BlogPostPage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GitHubPagesExample.Pages.Blog
+{
+    public class BlogPostPage : ComponentBase
+    {
+        public string PostTitle { get; set; }
+
+        public DateTime PostDate { get; set; }
+    }
+}
