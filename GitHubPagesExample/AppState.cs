f67f0d35a71f8e9944d0014d4773d9e2845136b6
Rob Mackenzie <rob@jeraconcepts.com>
2023-02-28 08:20:20 -0500

    Theme toggle

---
+++ GitHubPagesExample/AppState.cs
@@ -0,0 +1,13 @@
+﻿using GitHubPagesExample.Pages.Blog.Posts;
+
+namespace GitHubPagesExample
+{
+    public class AppState
+    {
+        public bool IsDarkMode;
+        public MudBlazor.Color AppBarColor => IsDarkMode ? MudBlazor.Color.Default : MudBlazor.Color.Primary;
+
+        public event Action StateChanged;
+        public void NotifyStateChanged() => StateChanged?.Invoke();
+    }
+}
--- GitHubPagesExample/Program.cs
+++ GitHubPagesExample/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GitHubPagesExample;
 using MudBlazor.Services;
-using GitHubPagesExample.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped<AppState>();
 
 await builder.Build().RunAsync();
