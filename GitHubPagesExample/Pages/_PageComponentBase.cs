9faf01b73f609cfb881f09b207e4ba66cff6cbcc
Rob Mackenzie <rob@jeraconcepts.com>
2023-03-02 11:58:09 -0500

    Cleanup

---
+++ GitHubPagesExample/Pages/_PageComponentBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GitHubPagesExample.Pages
+{
+    public abstract class PageComponentBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        public void NavigateTo(string url) => NavigationManager.NavigateTo(url);
+    }
+}
