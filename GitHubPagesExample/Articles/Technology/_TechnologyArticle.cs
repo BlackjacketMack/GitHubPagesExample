287eb66d872457c5d5189d8e0a55ec79178f8c8d
Rob Mackenzie <rob@jeraconcepts.com>
2023-03-02 15:19:42 -0500

    Added hangfire article

--- GitHubPagesExample/Articles/Technology/_TechnologyArticle.cs
+++ GitHubPagesExample/Articles/Technology/_TechnologyArticle.cs
@@ -6,5 +6,12 @@ namespace GitHubPagesExample.Pages
     {
        public string? Image { get; set; }
        public string? Icon { get; set; }
+       public string? ProjectUrl{get; set;}
+
+        //e.g. NuGet
+       public string? PackageUrl{get; set;}
+
+        // GitHub
+       public string? SourceUrl{get; set;}
     }
 }
